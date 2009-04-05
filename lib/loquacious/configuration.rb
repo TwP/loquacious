f71cfe44d9b95cf0b4ceb9b59da98beedf16125d
Tim Pease <tim.pease@gmail.com>
2009-04-05 14:03:18 -0600

    No longer undefining object_id. Ruby 1.9 gets very upset if it is removed.

--- lib/loquacious/configuration.rb
+++ lib/loquacious/configuration.rb
@@ -49,7 +49,7 @@ module Loquacious
       alias :help :help_for
     end
 
-    exceptions = %w[instance_of? kind_of? equal?]
+    exceptions = %w[instance_of? kind_of? equal? object_id]
     instance_methods.each do |m|
       undef_method m unless m[%r/^__/] or exceptions.include? m.to_s
     end
@@ -161,7 +161,7 @@ module Loquacious
       alias :__instance_eval :instance_eval
 
       instance_methods.each do |m|
-        undef_method m unless m[%r/^__/]
+        undef_method m unless m[%r/^__/] or m.to_s == 'object_id'
       end 
 
       # Create a new DSL and evaluate the given _block_ in the context of
